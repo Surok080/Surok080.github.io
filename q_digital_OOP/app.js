257056f978c16e1bb4f10ed1ec971fa54b4763ea
Alexander <sashakrvv@yandex.ru>
2022-04-27 19:09:06 +0400

    Update app.js, tab > space

--- q_digital_OOP/app.js
+++ q_digital_OOP/app.js
@@ -1,218 +1,218 @@
 class FuelValue {
-	value; // статус топлива 0- нет топлива, 1- есть топливо
-	constructor() {
-		this.value = 0;
-	}
-	fuelQuantity(value) {
-		this.value = value;
-	}
+   value; // статус топлива 0- нет топлива, 1- есть топливо
+   constructor() {
+      this.value = 0;
+   }
+   fuelQuantity(value) {
+      this.value = value;
+   }
 }
 class Airplane {
-	#name; //название самолета
-	#speed; // максимальная скорость самолета
-	static #counter = 0;
-	#num; // каунтер для добавки к имени самолета
-	#status; // статус самолета
-	#currentAirport; // Объект аэропорта в котором находится самолет
-	#fuel; // Объект параметра топлива у самолета
+   #name; //название самолета
+   #speed; // максимальная скорость самолета
+   static #counter = 0;
+   #num; // каунтер для добавки к имени самолета
+   #status; // статус самолета
+   #currentAirport; // Объект аэропорта в котором находится самолет
+   #fuel; // Объект параметра топлива у самолета
 
-	constructor(name, speed) {
-		this.#fuel = new FuelValue();
-		this.#name = name;
-		this.#speed = speed;
-		this.#status = 1;
-		this.#num = Airplane.#counter;
-		this.#currentAirport = '';
-		Airplane.#counter++;
-	}
-	fuelStatus(value) {
-		return this.#fuel.fuelQuantity(value);
-	}
-	//------------------------
-	land(airport) {
-		airport.landAirplane(this);
-	}
-	takeoff(airport) {
-		airport.takeoffAirplane(this);
-	}
-	takeoffFreed(airport) {
-		airport.takeoffFreedAirport(this);
-	}
-	//------------------------
-	get fuel() {
-		return this.#fuel;
-	}
-	get name() {
-		return this.#name;
-	}
-	set name(name) {
-		if (name === '') {
-			throw new Error('name field cannot be empty');
-		} else {
-			this.#name = name;
-		}
+   constructor(name, speed) {
+      this.#fuel = new FuelValue();
+      this.#name = name;
+      this.#speed = speed;
+      this.#status = 1;
+      this.#num = Airplane.#counter;
+      this.#currentAirport = '';
+      Airplane.#counter++;
+   }
+   fuelStatus(value) {
+      return this.#fuel.fuelQuantity(value);
+   }
+   //------------------------
+   land(airport) {
+      airport.landAirplane(this);
+   }
+   takeoff(airport) {
+      airport.takeoffAirplane(this);
+   }
+   takeoffFreed(airport) {
+      airport.takeoffFreedAirport(this);
+   }
+   //------------------------
+   get fuel() {
+      return this.#fuel;
+   }
+   get name() {
+      return this.#name;
+   }
+   set name(name) {
+      if (name === '') {
+         throw new Error('name field cannot be empty');
+      } else {
+         this.#name = name;
+      }
 
-	}
-	get statusAirplane() {
-		return this.#status
-	}
-	set statusAirplane(status) {
+   }
+   get statusAirplane() {
+      return this.#status
+   }
+   set statusAirplane(status) {
 
-		if (status === 0 || status === 1 || status === 2 || status === 3) {
-			this.#status = status;
-		} else {
-			throw new Error('name field cannot be empty');
-		}
+      if (status === 0 || status === 1 || status === 2 || status === 3) {
+         this.#status = status;
+      } else {
+         throw new Error('name field cannot be empty');
+      }
 
 
-	}
-	get currentAirport() {
-		return this.#currentAirport
-	}
-	set currentAirport(currentAirport) {
-		this.#currentAirport = currentAirport;
-	}
+   }
+   get currentAirport() {
+      return this.#currentAirport
+   }
+   set currentAirport(currentAirport) {
+      this.#currentAirport = currentAirport;
+   }
 
-	refueling() {
-		this.fuelStatus(1);
-		alert(`${this.name} заправлен`)
-	}
+   refueling() {
+      this.fuelStatus(1);
+      alert(`${this.name} заправлен`)
+   }
 
-	readyToFly() {
-		this.#status = 3;
-	}
+   readyToFly() {
+      this.#status = 3;
+   }
 
-	parkingAirplane() {
-		this.#status = 2;
-	}
+   parkingAirplane() {
+      this.#status = 2;
+   }
 
-	getNum() {
-		return this.#num;
-	}
-	getStatus() {
-		if (this.#status == 0) {
-			alert(`${this.name} находится на земле`)
-		} else if (this.#status == 1) {
-			alert(`${this.name} находится в воздухе`)
-		} else if (this.#status == 2) {
-			alert(`${this.name} находится на стоянке`)
-		} else if (this.#status == 3) {
-			alert(`${this.name} готов к взлету`)
-		}
-		return this.#status;
-	}
+   getNum() {
+      return this.#num;
+   }
+   getStatus() {
+      if (this.#status == 0) {
+         alert(`${this.name} находится на земле`)
+      } else if (this.#status == 1) {
+         alert(`${this.name} находится в воздухе`)
+      } else if (this.#status == 2) {
+         alert(`${this.name} находится на стоянке`)
+      } else if (this.#status == 3) {
+         alert(`${this.name} готов к взлету`)
+      }
+      return this.#status;
+   }
 }
 
 //Класс самолета МИГ----------------
 class Mig extends Airplane {
-	constructor(name, speed) {
-		super(name, speed);
-		this.name = name + '_' + this.getNum();
-	}
-	attack() {
-		//Проверку добавить в воздухе или нет
-		alert(this.name + ' атакует - ATTACK ');
-	}
+   constructor(name, speed) {
+      super(name, speed);
+      this.name = name + '_' + this.getNum();
+   }
+   attack() {
+      //Проверку добавить в воздухе или нет
+      alert(this.name + ' атакует - ATTACK ');
+   }
 }
 
 //Класс самолета Ту154----------------
 class Tu154 extends Airplane {
-	constructor(name, speed) {
-		super(name, speed);
-		this.name = name + '_' + this.getNum();
-	}
+   constructor(name, speed) {
+      super(name, speed);
+      this.name = name + '_' + this.getNum();
+   }
 }
 
 //Класс Аэропорта------------------
 class Airport {
-	#airplaneToAirport = []; //самолёты расположенные в аэропорту
-	#statusLand; //готовность аэропорта к отправке и принятию самолёта на взлёт 
-	#name; // наименование аэропорта
+   #airplaneToAirport = []; //самолёты расположенные в аэропорту
+   #statusLand; //готовность аэропорта к отправке и принятию самолёта на взлёт 
+   #name; // наименование аэропорта
 
-	constructor(name) {
-		this.#name = name
-		this.#airplaneToAirport = [];
-		this.#statusLand = false;
-	}
+   constructor(name) {
+      this.#name = name
+      this.#airplaneToAirport = [];
+      this.#statusLand = false;
+   }
 
-	get name() {
-		return this.#name;
-	}
-	get airplaneToAirport() {
-		return this.#airplaneToAirport
-	}
-	set airplaneToAirport(airplaneToAirport) {
-		this.#airplaneToAirport = airplaneToAirport;
-	}
+   get name() {
+      return this.#name;
+   }
+   get airplaneToAirport() {
+      return this.#airplaneToAirport
+   }
+   set airplaneToAirport(airplaneToAirport) {
+      this.#airplaneToAirport = airplaneToAirport;
+   }
 
-	set statusLand(statusLand) {
-		this.#statusLand = statusLand;
-	}
+   set statusLand(statusLand) {
+      this.#statusLand = statusLand;
+   }
 
-	onReadyToAccept() {
-		this.#statusLand = false;
-	}
+   onReadyToAccept() {
+      this.#statusLand = false;
+   }
 
-	readyToAccept() {
-		this.#statusLand = true;
-	}
+   readyToAccept() {
+      this.#statusLand = true;
+   }
 
-	airportBan(airplane) {
-		alert(`Запрет на взлет и посадку ${airplane.name} , Аэропорт - ${this.name} не готов`);
-	}
-	takeoffFreedAirport(airplane) {
-		airplane.readyToFly();
-		this.readyToAccept();
-		this.takeoffAirplane(airplane);
-	}
+   airportBan(airplane) {
+      alert(`Запрет на взлет и посадку ${airplane.name} , Аэропорт - ${this.name} не готов`);
+   }
+   takeoffFreedAirport(airplane) {
+      airplane.readyToFly();
+      this.readyToAccept();
+      this.takeoffAirplane(airplane);
+   }
 
-	statusLand() {
-		if (!this.#statusLand) {
-			alert(`Аэропорт ${this.name} не готов к принятию и отправке`)
-		} else {
-			alert(`Аэропорт ${this.name} готов к принятию и отправке`)
-		}
-	}
+   statusLand() {
+      if (!this.#statusLand) {
+         alert(`Аэропорт ${this.name} не готов к принятию и отправке`)
+      } else {
+         alert(`Аэропорт ${this.name} готов к принятию и отправке`)
+      }
+   }
 
-	landAirplane(airplane) {
-		if (airplane.statusAirplane == 1) {
-			if (this.#statusLand) {
-				this.airplaneToAirport.push(airplane);
-				airplane.currentAirport = this;
-				this.onReadyToAccept(); //Статус аэропорта переходит в - "не готов", т.к. полоса занята посадкой
-				airplane.statusAirplane = 0;
-				airplane.fuelStatus(0); // Статус топлива самолета переводится на 0 (отсутствует)
-			} else {
-				this.airportBan(airplane)
-			}
-		} else {
-			alert(`Посадка не возможна`);
-			airplane.getStatus()
-		}
+   landAirplane(airplane) {
+      if (airplane.statusAirplane == 1) {
+         if (this.#statusLand) {
+            this.airplaneToAirport.push(airplane);
+            airplane.currentAirport = this;
+            this.onReadyToAccept(); //Статус аэропорта переходит в - "не готов", т.к. полоса занята посадкой
+            airplane.statusAirplane = 0;
+            airplane.fuelStatus(0); // Статус топлива самолета переводится на 0 (отсутствует)
+         } else {
+            this.airportBan(airplane)
+         }
+      } else {
+         alert(`Посадка не возможна`);
+         airplane.getStatus()
+      }
 
-	}
-	takeoffAirplane(airplane) {
-		console.log(airplane.fuel.value);
-		if (airplane.statusAirplane == 3 && airplane.fuel.value == 1) {
-			if (this.#statusLand) {
-				for (let i = 0; i < this.airplaneToAirport.length; i++) {
-					if (this.airplaneToAirport[i].name == airplane.name) {
-						this.airplaneToAirport.splice(i, 1)
-						airplane.currentAirport = '';
-						alert(`Взлет ${airplane.name} выполнен `);
-					}
-				}
-				airplane.statusAirplane = 1;
-			} else {
-				this.airportBan(airplane)
-			}
-		} else if (airplane.fuel.value == 0) {
-			alert(`Взлет не возможен, ${airplane.name} требуется заправка топлива `);
-		} else {
-			alert(`Взлет не возможен`);
-			airplane.getStatus()
-		}
-	}
+   }
+   takeoffAirplane(airplane) {
+      console.log(airplane.fuel.value);
+      if (airplane.statusAirplane == 3 && airplane.fuel.value == 1) {
+         if (this.#statusLand) {
+            for (let i = 0; i < this.airplaneToAirport.length; i++) {
+               if (this.airplaneToAirport[i].name == airplane.name) {
+                  this.airplaneToAirport.splice(i, 1)
+                  airplane.currentAirport = '';
+                  alert(`Взлет ${airplane.name} выполнен `);
+               }
+            }
+            airplane.statusAirplane = 1;
+         } else {
+            this.airportBan(airplane)
+         }
+      } else if (airplane.fuel.value == 0) {
+         alert(`Взлет не возможен, ${airplane.name} требуется заправка топлива `);
+      } else {
+         alert(`Взлет не возможен`);
+         airplane.getStatus()
+      }
+   }
 }
 
 
