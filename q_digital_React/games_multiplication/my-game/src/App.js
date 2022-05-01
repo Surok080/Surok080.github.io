9c1ae4a91154e80ad903c9ee48f0647f1d6df5dc
Alexander <sashakrvv@yandex.ru>
2022-05-01 09:45:11 +0400

    redistructorize

--- q_digital_React/games_multiplication/my-game/src/App.js
+++ q_digital_React/games_multiplication/my-game/src/App.js
@@ -1,18 +1,19 @@
-import {GamePage} from 'pages/GamePage';
-import { Routes, Route} from 'react-router-dom'
-import { HomePage } from './pages/HomePage';
-import { LoginPage } from './pages/LoginPage';
-import { RegisterPage } from './pages/RegisterPage';
+
+import LoginPage from 'pages/LoginPage';
+import { Routes, Route } from 'react-router-dom'
+
 
 
 
 function App() {
 	return (
 		<Routes>
-			<Route path='/' element={<HomePage />} />
+			<Route path='/' element={<LoginPage />} />
+			{/* <Route path='/' element={<HomePage />} />
 			<Route path='/login' element={<LoginPage />} />
+			<Route path='/loginOne' element={<LoginPageOne />} />
 			<Route path='/register' element={<RegisterPage />} />
-			<Route path='/game' element={<GamePage />} />
+			<Route path='/game' element={<GamePage />} /> */}
 		</Routes>
 	);
 }
